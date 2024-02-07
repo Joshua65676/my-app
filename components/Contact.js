c08d0efcba1d2fd29a4b020ee676137bf47576a2
Okoro Joshua <Joshua65676@gmail.com>
2024-02-07 15:10:48 +0100

    first commit

---
+++ components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Form } from 'react-router-dom'
+
+export default function Contact() {
+  return (
+    <section>
+      <div>
+        <Form></Form>
+      </div>
+    </section>
+  )
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,17 @@
+export default function Contact(){
+    return(
+        <section>
+            <div>
+                <div class="mr-20 -ml-16 pt-10 mt-36 md:p-20 p-32 md:w-4/12 bg-white md:pt-1 md:ml-40">
+                  <div class="md:mt-10 md:-ml-5 -ml-10"> 
+                    <span class="font-bold ml-8">Get In Touch</span>
+                  </div>
+                    <hr class="About-line bg-red-600 p-1 w-40 h-2 m-12 -ml-28"/>
+                  <div class="md:m-20 md:-ml-1 -ml-5">
+                    <h2 class="font-bold md:text-7xl text-3xl md:-mr-28">Contact</h2>
+                  </div>
+                </div>
+            </div>
+        </section>
+    )
+}
