3e137a0a11bc055e99c3bf2d1878672e549b6228
Okoro Joshua <Joshua65676@gmail.com>
2024-05-25 11:55:04 +0100

    first commit

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -14,35 +14,35 @@ export default function Sidebar() {
     return (
       <div class="lg:hidden">
           <div class="flex justify-between">
-            <Link href='/'> 
+            <Link href='/'>
               <Image class="w-28 p-50 pt-3 pb-10 ml-5" src={logo} alt="logo" />
              </Link>
-            {open ? 
-              (<Link href="#" class="mr-5 pt-6"> <AiOutlineClose class="bg-red-600 w-10 h-10 "  onClick={showSideBar}/> </Link>)
+            {open ?
+              (<Link href="#" class="mr-5 pt-6"> <AiOutlineClose class=" w-10 h-10 "  onClick={showSideBar}/> </Link>)
             :
-              (  <Link href="#" class="mr-5 pt-6 "> <IoMenuSharp class="bg-red-600 w-10 h-10 "  onClick={showSideBar}/> </Link>)
+              (  <Link href="#" class="mr-5 pt-6 "> <IoMenuSharp class=" w-10 h-10 "  onClick={showSideBar}/> </Link>)
             }
           </div>
          {open && (
             <div>
              <ul class=" pl- pt-8 pb-8 space-y-5 divide-y divide-slate-400 bg-white">
                <li class="pl-5">
-               <Link href='/' class="hover:text-red-600 text-sm font-normal"> HOME </Link>
+               <Link href='/' class="hover:text-red-600 text-sm font-bold"> HOME </Link>
                </li>
                <li class="pl-5 pt-5">
-               <Link href='/about' class="hover:text-red-600 text-sm font-normal"> ABOUT </Link>
+               <Link href='/about' class="hover:text-red-600 text-sm font-bold"> ABOUT </Link>
                </li>
                <li class="pl-5 pt-5">
-               <Link href='/services' class="hover:text-red-600 text-sm font-normal"> SERVICES </Link>
+               <Link href='/services' class="hover:text-red-600 text-sm font-bold"> SERVICES </Link>
                </li>
                <li class="pl-5 pt-5">
-               <Link href='/projects' class="hover:text-red-600 text-sm font-normal"> PROJECTS </Link>
+               <Link href='/projects' class="hover:text-red-600 text-sm font-bold"> PROJECTS </Link>
                </li>
                <li class="pl-5 pt-5">
-               <Link href='/clients' class="hover:text-red-600 text-sm font-normal"> CLIENTS </Link>
+               <Link href='/clients' class="hover:text-red-600 text-sm font-bold"> CLIENTS </Link>
                </li>
                <li class="pl-5 pt-5">
-               <Link href='/contact' class="hover:text-red-600 text-sm font-normal"> CONTACT </Link>
+               <Link href='/contact' class="hover:text-red-600 text-sm font-bold"> CONTACT </Link>
                </li>
              </ul>
             </div>
